0f788de4b5a73939cefffc04a3ea3bf06bcc35d6
Piotr <Piotr@192.168.0.17>
2023-03-14 14:45:34 +0100

    Creating the game window

--- src/main/java/asteroids/AsteroidsApplication.java
+++ src/main/java/asteroids/AsteroidsApplication.java
@@ -1,14 +1,31 @@
 package asteroids;
 
-public class AsteroidsApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class AsteroidsApplication extends Application{
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        launch(AsteroidsApplication.class);
     }
 
     public static int partsCompleted() {
         // State how many parts you have completed using the return value of this method
         return 0;
+    }
+
+    @Override
+    public void start(Stage win) throws Exception {
+        Pane layout = new Pane();
+        layout.setPrefSize(600, 400);
+        
+        Scene scene = new Scene(layout);
+        win.setTitle("Asteroids");
+        win.setScene(scene);
+        win.show();
     }
 
 }
